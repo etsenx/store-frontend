0635928e6da8faa729374942dd59674e8377650b
Steven <ststeven82@gmail.com>
2024-03-25 16:46:59 +0700

    Progress: Edit Product Image

--- src/pages/Admin/Products/EditImage/EditImage.jsx
+++ src/pages/Admin/Products/EditImage/EditImage.jsx
@@ -1,30 +1,197 @@
-import { InputText } from "primereact/inputtext";
-import Button from "../../../../components/Button/Button";
+import { Toast } from "primereact/toast";
+import { Tooltip } from "primereact/tooltip";
+import { FileUpload } from "primereact/fileupload";
+import { useRef, useState } from "react";
+import { ProgressBar } from "primereact/progressbar";
+import { Tag } from "primereact/tag";
+
+import "./EditImage.css";
+import { Button } from "primereact/button";
 
 function EditImage() {
+  const toast = useRef(null);
+  const [totalSize, setTotalSize] = useState(0);
+  const fileUploadRef = useRef(null);
+
+  const onTemplateUpload = (e) => {
+    let _totalSize = 0;
+
+    e.files.forEach((file) => {
+      _totalSize += file.size || 0;
+    });
+
+    setTotalSize(_totalSize);
+    toast.current.show({
+      severity: "info",
+      summary: "Success",
+      detail: "File Uploaded",
+    });
+  };
+
+  const onTemplateSelect = (e) => {
+    let _totalSize = totalSize;
+    let files = e.files;
+
+    Object.keys(files).forEach((key) => {
+      _totalSize += files[key].size || 0;
+    });
+
+    setTotalSize(_totalSize);
+  };
+
+  const onTemplateRemove = (file, callback) => {
+    setTotalSize(totalSize - file.size);
+    callback();
+  };
+
+  const onTemplateClear = () => {
+    setTotalSize(0);
+  };
+
+  const headerTemplate = (options) => {
+    const { className, chooseButton, uploadButton, cancelButton } = options;
+    const value = totalSize / 10000;
+    const formatedValue =
+      fileUploadRef && fileUploadRef.current
+        ? fileUploadRef.current.formatSize(totalSize)
+        : "0 B";
+
+    return (
+      <div
+        className={className}
+        style={{
+          backgroundColor: "transparent",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        {chooseButton}
+        {uploadButton}
+        {cancelButton}
+        <div className="flex align-items-center gap-3 ml-auto">
+          <span className="text-sm">{formatedValue} / 1 MB</span>
+          <ProgressBar
+            value={value}
+            showValue={false}
+            style={{ width: "10rem", height: "6px" }}
+          ></ProgressBar>
+        </div>
+      </div>
+    );
+  };
+
+  const itemTemplate = (file, props) => {
+    return (
+      <div className="flex align-items-center flex-wrap">
+        <div className="flex align-items-center" style={{ width: "40%" }}>
+          <img
+            alt={file.name}
+            role="presentation"
+            src={file.objectURL}
+            width={50}
+          />
+          <span className="text-sm flex flex-column text-left ml-3">
+            {file.name}
+            <small>{new Date().toLocaleDateString()}</small>
+          </span>
+        </div>
+        <Tag
+          value={props.formatSize}
+          severity="warning"
+          className="px-3 py-2"
+        />
+        <Button
+          type="button"
+          icon="pi pi-times"
+          className="cancel-button p-button-outlined p-button-rounded p-button-danger ml-auto"
+          onClick={() => onTemplateRemove(file, props.onRemove)}
+        />
+      </div>
+    );
+  };
+
+  const emptyTemplate = () => {
+    return (
+      <div className="flex align-items-center flex-column">
+        <i
+          className="pi pi-image mt-3 p-5"
+          style={{
+            fontSize: "5em",
+            borderRadius: "50%",
+            backgroundColor: "var(--surface-b)",
+            color: "var(--surface-d)",
+          }}
+        ></i>
+        <span
+          style={{ fontSize: "1.2em", color: "var(--text-color-secondary)" }}
+          className="my-5"
+        >
+          Drag and Drop Image Here
+        </span>
+      </div>
+    );
+  };
+
+  const chooseOptions = {
+    icon: "pi pi-fw pi-images",
+    iconOnly: true,
+    className: "upload-buttons image-choose-btn p-button-rounded p-button-outlined",
+  };
+  const uploadOptions = {
+    icon: "pi pi-fw pi-cloud-upload",
+    iconOnly: true,
+    className:
+      "upload-buttons image-upload-btn p-button-success p-button-rounded p-button-outlined",
+  };
+  const cancelOptions = {
+    icon: "pi pi-fw pi-times",
+    iconOnly: true,
+    className:
+      "upload-buttons image-cancel-btn p-button-danger p-button-rounded p-button-outlined",
+  };
+
   return (
     <div className="w-full">
-      <form className="add-product-form ml-5">
-        <h1 className="add-product-title">Upload Product Images</h1>
-        <label
-          className="block mb-2"
-          htmlFor="multiple_files"
-        >
-          Choose Images
-        </label>
-        <input
-          className="block w-full"
-          id="multiple_files"
-          type="file"
-          multiple
-        ></input>
-        <label htmlFor="product-name">Name</label>
-        <InputText
-          id="product-name"
-          type="text"
-          className="p-inputtext-sm p-0"
-        />
-        <Button className="add-product-button mt-3">CREATE</Button>
+      <form className="edit-product-image-form ml-5">
+        <h1 className="edit-product-image-title">Upload Product Images</h1>
+        <div>
+          <Toast ref={toast}></Toast>
+
+          <Tooltip
+            target=".custom-choose-btn"
+            content="Choose"
+            position="bottom"
+          />
+          <Tooltip
+            target=".custom-upload-btn"
+            content="Upload"
+            position="bottom"
+          />
+          <Tooltip
+            target=".custom-cancel-btn"
+            content="Clear"
+            position="bottom"
+          />
+
+          <FileUpload
+            ref={fileUploadRef}
+            name="demo[]"
+            url="/api/upload"
+            multiple
+            accept="image/*"
+            maxFileSize={1000000}
+            onUpload={onTemplateUpload}
+            onSelect={onTemplateSelect}
+            onError={onTemplateClear}
+            onClear={onTemplateClear}
+            headerTemplate={headerTemplate}
+            itemTemplate={itemTemplate}
+            emptyTemplate={emptyTemplate}
+            chooseOptions={chooseOptions}
+            uploadOptions={uploadOptions}
+            cancelOptions={cancelOptions}
+          />
+        </div>
       </form>
     </div>
   );
